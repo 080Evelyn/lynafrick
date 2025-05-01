4d4f90166b664b4d1f191be9772eeec00e9af8ae
NEDU HILARY <chineduhilary80@gmail.com>
2025-05-01 12:24:03 +0100

    Worked on mobile  Humburger menu and hero section

--- lynafrick/src/Components/NavBar.jsx
+++ lynafrick/src/Components/NavBar.jsx
@@ -1,30 +1,60 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react'
+
+import { NavLink, Link } from 'react-router-dom';
 import { IoIosMenu } from "react-icons/io";
 
 function NavBar() {
+
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  
+
+
   return (
-    <div className='flex justify-between items-center px-[16px] md:px-[94.5px] bg-white md:py-[23px]'>
-      <div>
-        <h1 className='poppins-semibold text-[20px] md:text-[30px] text-[#00D094]'>LYN</h1>
-      </div>
+    <div>
 
-      <div>
-        <IoIosMenu className='w-[24px] h-[24px] md:hidden'/>
-        <ul className='hidden md:flex gap-[20px] items-center text-[#01282F]'>
-                <li><NavLink to="/home" className='hover:font-semibold'>Home</NavLink></li>
-                <li><NavLink to="/product" className='hover:font-semibold'>Product</NavLink></li>
-                <li><NavLink to="/about-us" className='hover:font-semibold'>About Us</NavLink></li>
-                <li><NavLink to="/contact-us" className='hover:font-semibold'>Contact Us</NavLink></li>
-                <li><NavLink to="/blog" className='hover:font-semibold'>Blog</NavLink></li>
-        </ul>
-      </div>
+      {/* Hamburger Icon */}
+      <div className='flex justify-between items-center px-[16px] md:px-[94.5px] bg-gray-200 md:py-[23px]'>
+        <div>
+          <h1 className='poppins-semibold text-[20px] md:text-[30px] text-[#00D094]'>LYN</h1>
+        </div>
 
-      <div className='hidden md:flex items-center gap-[7px]'>
-        <button className='hidden md:flex text-[#01282F] font-semibold'>Sign Up</button>
-        <button className='hidden md:flex bg-[#01282F] text-white font-semibold px-[20px] py-[2px]'>Login</button>
+        <div>
+          
+          <ul className='hidden md:flex gap-[20px] items-center text-[#01282F]'>
+                  <li><NavLink to="/home" className='hover:font-semibold'>Home</NavLink></li>
+                  <li><NavLink to="/product" className='hover:font-semibold'>Product</NavLink></li>
+                  <li><NavLink to="/about-us" className='hover:font-semibold'>About Us</NavLink></li>
+                  <li><NavLink to="/contact-us" className='hover:font-semibold'>Contact Us</NavLink></li>
+                  <li><NavLink to="/blog" className='hover:font-semibold'>Blog</NavLink></li>
+          </ul>
+        </div>
+
+        <div className='hidden md:flex items-center gap-[7px]'>
+          <button className='md:flex text-[#01282F] font-semibold'>Sign Up</button>
+          <button className='md:flex bg-[#01282F] text-white font-semibold px-[20px] py-[2px]'>Login</button>
+        </div>
+
+        {/* Hamburger Icon */}
+        <IoIosMenu className='w-[24px] h-[24px] md:hidden cursor-pointer' onClick={handleClick}/>
+
       </div>
+        {/* Mobile Menu */}
+        {click && (
+        <div className='left-7 bg-gray-200 shadow-lg w-full md:hidden z-50'>
+          <ul className='flex flex-col gap-[10px] items-start pl-3 text-[#01282F] py-[20px]'>
+            <li><NavLink to="/home" className='hover:font-semibold' onClick={() => setClick(false)}>Home</NavLink></li>
+            <li><NavLink to="/product" className='hover:font-semibold' onClick={() => setClick(false)}>Product</NavLink></li>
+            <li><NavLink to="/about-us" className='hover:font-semibold' onClick={() => setClick(false)}>About Us</NavLink></li>
+            <li><NavLink to="/contact-us" className='hover:font-semibold' onClick={() => setClick(false)}>Contact Us</NavLink></li>
+            <li><NavLink to="/blog" className='hover:font-semibold' onClick={() => setClick(false)}>Blog</NavLink></li>
+          </ul>
+        </div>
+      )}
     </div>
+    
   )
 }
 
--- lynafrick/src/Components/pages/Home.jsx
+++ lynafrick/src/Components/pages/Home.jsx
@@ -1,59 +1,135 @@
 import React from 'react'
 import Image from '../../assets/Images/image.png'
 import Arrowicon from '../../assets/circle.svg'
-import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import Arrowicon2 from '../../assets/circleg.svg'
+import Frame from '../../assets/Frame.svg'
+import image1 from '../../assets/Images/kimage.png'
 
 
 function Home() {
   return (
-    <div className='relative w-full h-[310px] md:h-[565px]'>
-        <img src={Image} alt="" className='w-full h-[310px] md:h-[565px] object-cover'/>
 
-        <div className='absolute inset-0 flex flex-col text-[#FFFFFF] text-center top-[80px]'>
-        
-            <h1 className='h-[44px] font-bold text-[20px] md:text-[48px] leading-[22px] md:leading-[50px]'>
-                <span className="block">Home Meal Away</span>
-                <span className="block">From Home</span>
-            </h1>
+    <div className='bg-gray-200'>
 
-            <p className='text-[10px] tracking-tight md:text-[24px] pt-2 md:pt-16 text-center md:leading-[26px]'>
-                <span className=''>"Enjoy the rich taste of Nigerian cuisine with handpicked </span><br />
-                <span className=''>ingredients, ensuring every meal you cook reflects the culture,</span><br />
-                <span className=''>heritage, and flavors of home.</span>
-            </p>
+        <div className='relative w-full h-[310px] md:h-[500px] bg-gray-200'>
+            <img src={Image} alt="" className='w-full h-[290px] md:h-[445px] object-cover brightness-50'/>
 
-            <div className='flex justify-center space-x-4 pt-[10px] text-[10px] md:text-[24px] md:pt-[30px]'>
-                <button className='flex space-x-4 items-center border-0 p-[5px] bg-[#01282F]'>
-                    <span className='flex items-center space-x-2'>
-                        <span>Get Started</span>
-                        <img src={Arrowicon} alt="" className='text-[10px] md:text-[20px]'/>
-                    </span>
-                </button>
+            <div className='absolute inset-0 flex flex-col text-[#FFFFFF] text-center top-[80px]'>
+            
+                <h1 className='h-[44px] font-bold text-[20px] md:text-[48px] leading-[22px] md:leading-[50px]'>
+                    <span className="block">Home Meal Away</span>
+                    <span className="block">From Home</span>
+                </h1>
 
-                <button className='flex items-center border border-[#00D094] p-[5px]'>
-                    <span className='flex items-center space-x-2'>
-                        <span className='text-[#00D094]'>Learn more</span>
-                        <img src={Arrowicon} alt="" className='text-[10px] md:text-[20px] color-[#00D094]'/>
-                    </span>
-                </button>
-            </div>
+                <p className='text-[10px] tracking-tight md:text-[24px] pt-2 md:pt-16 text-center md:leading-[26px]'>
+                    <span className=''>"Enjoy the rich taste of Nigerian cuisine with handpicked </span><br />
+                    <span className=''>ingredients, ensuring every meal you cook reflects the culture,</span><br />
+                    <span className=''>heritage, and flavors of home.</span>
+                </p>
 
-            <div className='flex bg-[#FFFFFF] w-[380px] h-[41px]'>
-                <div className='flex'>
-                    <button>
-                        Categories
+                <div className='flex justify-center space-x-4 pt-[10px] text-[10px] md:text-[24px] md:pt-[30px]'>
+                    <button className='flex space-x-4 items-center border-0 p-[5px] bg-[#01282F]'>
+                        <span className='flex items-center space-x-2'>
+                            <span>Get Started</span>
+                            <img src={Arrowicon} alt="" className='w-[10px] md:w-[20px]'/>
+                        </span>
                     </button>
 
-                    <div></div>
-
-                    <button>
-                        Search
+                    <button className='flex items-center border border-[#00D094] p-[5px]'>
+                        <span className='flex items-center space-x-2'>
+                            <span className='text-[#00D094]'>Learn more</span>
+                            <img src={Arrowicon2} alt="" className='w-[10px] md:w-[20px] color-[#00D094]'/>
+                        </span>
                     </button>
                 </div>
+
+                <div className='flex mx-auto bg-[#FFFFFF] w-[300px] text-[10px] md:text-[24px] h-[41px] mt-3 md:w-[800px] md:h-[50px]'>
+                    <div className='flex space-x-2 p-2 md:space-x-4'>
+                        <button className=''>
+                            <span className='border border-[#000000] text-[#000000] py-[4px] px-[5px] md:py-[1px] md:px-[7px]'>
+                                Categories
+                            </span>
+                        </button>
+
+                        <div className='border border-[#000000] w-[159px] md:w-[531px]'></div>
+
+                        <button className='bg-[#01282F] py-[4px] px-[7px] md:py-[1px] md:px-[10px]'>
+                            <span className='text-[#FFFFFF]'>
+                                Search
+                            </span>
+                        </button>
+                    </div>
+                </div>
+        
             </div>
+
+        </div>
+
+
+        <div className='grid grid-cols-2 px-3  md:px-6 font-montserrat py-2 bg-[#FFFFFF]'>
+            <div className='grid gap-y-[6px] md:gap-y-7 md:py-[40px]  leading-[9px] '>
+                <span className='text-[#01282F] text-[10px] md:text-[20px]'>
+                    Reason You need Us
+                </span>
+
+                <h1 className='font-bold text-[#130F26] leading-[15px] md:leading-[48px] md:text-[50px]'>
+                    One Platform For 
+                    Nigeria Culture
+                </h1>
+
+                <p className='text-[10px] md:text-[20px] leading-[10px] md:leading-[20px] text-[#000000]'>
+                    Nigerian expatriates and food enthusiasts worldwide can reconnect with the flavors of home. 
+                    The platform ensures easy access to traditional ingredients
+                </p>
+
+                <span className='flex items-center space-x-2'>
+                    <img src={Frame} alt="" className='md:w-[30px]'/> 
+                    <p className='font-[poppins-regular] text-[10px] md:text-[20px]'>
+                        Convenience and Accessibility
+                    </p>
+                </span>
+
+                <span className='flex items-center space-x-2'>
+                    <img src={Frame} alt="" className='md:w-[30px]'/> 
+                    <p className='font-[poppins-regular] text-[10px] md:text-[20px]'>
+                        Support for Cultural Connections
+                    </p>
+                </span>
+
+                <span className='flex items-center space-x-2'>
+                    <img src={Frame} alt="" className='md:w-[30px]'/> 
+                    <p className='font-[poppins-regular] text-[10px] md:text-[20px]'>
+                        Quality Assurance and Trusted Sourcing
+                    </p>
+                </span>
+            </div>
+
+            <div className="relative w-fit max-w-md mx-auto mt-10 left-[10px]">
+                {/* Background square (behind image) */}
+                <div className="absolute top-[-12px] right-[-3px] lg:right-[-70px] w-[100px] h-[120px] lg:h-[220px] bg-[#01282F] z-0 lg:w-[300px]"></div>
+
+                {/* Foreground image */}
+                <img
+                    src={image1}
+                    alt="Family cooking"
+                    className="relative z-10 w-[160px] lg:w-[400px] h-auto object-cover right-[5px] lg:right-[-50px]"
+                />
+            </div>
+
+
         </div>
     </div>
   )
 }
 
 export default Home;
+
+
+<div className='relative w-full flex justify-center items-center px-3 md:px-20 mt-6'>
+
+<div className='relative w-full left-2 max-w-[500px] md:left-20 md:max-w-[700px]'>
+    <div className='bg-[#01282F] w-[110px] md:w-[300px] h-[100px] md:h-[300px] absolute top-1 md:top-5 left-[60px] md:left-[200px] z-0 '>
+        <img src={image1} alt="" className='relative z-10 right-[10px] top-3 w-full md:w-[400px] h-[150px] md:h-[300px]'/>
+    </div>
+</div>
+</div>
