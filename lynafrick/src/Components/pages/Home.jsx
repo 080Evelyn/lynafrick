9ebce1709d234420a5cb329fe94f3662300e472f
NEDU HILARY <chineduhilary80@gmail.com>
2025-05-01 17:57:36 +0100

    Added Category Images

--- lynafrick/src/Components/pages/Home.jsx
+++ lynafrick/src/Components/pages/Home.jsx
@@ -4,6 +4,12 @@ import Arrowicon from '../../assets/circle.svg'
 import Arrowicon2 from '../../assets/circleg.svg'
 import Frame from '../../assets/Frame.svg'
 import image1 from '../../assets/Images/kimage.png'
+import  imageSt from '../../assets/Images/image (2).png'
+import imageSo from '../../assets/Images/Soup1.png'
+import imageSo1 from '../../assets/Images/Mask Group.png'
+import imageGro from '../../assets/Images/Mask Group (1).png'
+import imageSo2 from '../../assets/Images/image (3).png'
+import imageSo3 from '../../assets/Images/Mask Group (2).png'
 
 
 function Home() {
@@ -43,7 +49,7 @@ function Home() {
                     </button>
                 </div>
 
-                <div className='flex mx-auto bg-[#FFFFFF] w-[300px] text-[10px] md:text-[24px] h-[41px] mt-3 md:w-[800px] md:h-[50px]'>
+                <div className='flex mx-auto bg-[#FFFFFF] w-[300px] text-[10px] md:text-[20px] h-[41px] mt-3 md:w-[800px] md:h-[50px]'>
                     <div className='flex space-x-2 p-2 md:space-x-4'>
                         <button className=''>
                             <span className='border border-[#000000] text-[#000000] py-[4px] px-[5px] md:py-[1px] md:px-[7px]'>
@@ -84,21 +90,21 @@ function Home() {
 
                 <span className='flex items-center space-x-2'>
                     <img src={Frame} alt="" className='md:w-[30px]'/> 
-                    <p className='font-[poppins-regular] text-[10px] md:text-[17px] lg:text-[20px]'>
+                    <p className='font-poppins text-[10px] md:text-[17px] lg:text-[20px]'>
                         Convenience and Accessibility
                     </p>
                 </span>
 
                 <span className='flex items-center space-x-2'>
                     <img src={Frame} alt="" className='md:w-[30px]'/> 
-                    <p className='font-[poppins-regular] text-[10px] md:text-[17px] lg:text-[20px]'>
+                    <p className='font-poppins text-[10px] md:text-[17px] lg:text-[20px]'>
                         Support for Cultural Connections
                     </p>
                 </span>
 
                 <span className='flex items-center space-x-2'>
                     <img src={Frame} alt="" className='md:w-[30px]'/> 
-                    <p className='font-[poppins-regular] text-[10px] md:text-[17px] lg:text-[20px]'>
+                    <p className='font-poppins-regular text-[10px] md:text-[17px] lg:text-[20px]'>
                         Quality Assurance and Trusted Sourcing
                     </p>
                 </span>
@@ -112,11 +118,55 @@ function Home() {
                 <img
                     src={image1}
                     alt="Family cooking"
-                    className="relative z-10 w-[160px] md:w-[400px] h-auto object-cover right-[5px] md:right-[-10px] lg:right-[-50px]"
+                    className="relative brightness-50 z-10 w-[160px] md:w-[400px] h-auto object-cover right-[5px] md:right-[-10px] lg:right-[-50px]"
                 />
             </div>
 
+        </div>
 
+        <div>
+            <div className='flex flex-col items-center justify-center text-center py-4 md:py-10 text-[#01282F]'>
+                <h3 className='font-bold font-poppins md:text-[24px]'>Browse from Our Categories</h3>
+                <p className='text-[10px] font-poppins md:text-[20px]'>We bring Ingredient from all part of Nigeria</p>
+            </div>
+
+            <div className='grid grid-cols-2 md:grid-cols-3 gap-6 px-3 md:px-20 py-6 gap-y-9 text-center'>
+                <div>
+                    <img src={imageSt} alt="" className='rounded-lg'/>
+                    <h3 className='font-poppins font-bold text-[#01282F] text-[15px] md:text-[24px]'>North West Region</h3>
+                    <p className='text-[10px] md:text-[20px] leading-[10px] md:leading-[20px] text-gray-600'>Offering You the best fromKano, Kaduna, Katsina, Sokoto, Jigawa, Kebbi, Zamfara</p>
+                </div>
+
+                <div>
+                    <img src={imageSo} alt="" className='rounded-lg'/>
+                    <h3 className='font-poppins font-bold text-[#01282F] text-[15px] md:text-[24px]'>South South Region</h3>
+                    <p className='text-[10px] md:text-[17px] leading-[10px] md:leading-[15px] text-gray-600'>Offering You the best from  Rivers, Bayelsa, Cross River, Akwa Ibom, Edo, Delta</p>
+                </div>
+
+                <div>
+                    <img src={imageSo1} alt="" className='rounded-lg'/>
+                    <h3 className='font-poppins font-bold text-[#01282F] text-[15px] md:text-[24px]'>South East Region</h3>
+                    <p className='text-[10px] md:text-[17px] leading-[10px] md:leading-[15px] text-gray-600'>Offering You the best from Anambra, Enugu, Imo, Abia, Ebonyi</p>
+                </div>
+                <div>
+                    <img src={imageGro} alt="" className='rounded-lg'/>
+                    <h3 className='font-poppins font-bold text-[#01282F] text-[15px] md:text-[24px]'>South West Region</h3>
+                    <p className='text-[10px] md:text-[17px] leading-[10px] md:leading-[15px] text-gray-600'>Offering You the best from Lagos, Ogun, Oyo, Osun, Ondo, Ekiti</p>
+                </div>
+
+                <div>
+                    <img src={imageSo2} alt="" className='rounded-lg'/>
+                    <h3 className='font-poppins font-bold text-[#01282F] text-[15px] md:text-[24px]'>North Central Region</h3>
+                    <p className='text-[10px] md:text-[17px] leading-[10px] md:leading-[15px] text-gray-600'>Offering You the best from Kogi, Benue, Niger,
+                    Kwara, Plateau, Nasarawa, FCT Abuja</p>
+                </div>
+
+                <div>
+                    <img src={imageSo3} alt="" className='rounded-lg'/>
+                    <h3 className='font-poppins font-bold text-[#01282F] text-[15px] md:text-[24px]'>North East Region</h3>
+                    <p className='text-[10px] md:text-[17px] leading-[10px] md:leading-[15px] text-gray-600'>Offering You the best from Borno, Yobe, Taraba, Adamawa, Gombe, Bauchi</p>
+                </div>
+            </div>
         </div>
     </div>
   )
