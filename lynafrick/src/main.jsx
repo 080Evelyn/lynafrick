a839c0aec0eae8364a51124da909b0768a13525d
080Evelyn <oka4evelyn89@gmail.com>
2025-05-12 13:20:57 +0100

    changes

--- lynafrick/src/main.jsx
+++ lynafrick/src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-     <App />
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
 )
